b7d6da13b116c06b539f16ad861f64ce81559c84
elkinjimenez <elkin.jiga@hotmail.com>
2020-02-15 23:56:04 -0500

    Creando el menú hamburguesa

---
+++ src/app/Models/home/home.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  menu = { icono: 'far fa-times-circle', accionMenu: true };
+
+  constructor() { }
+
+  ngOnInit() {
+
+    this.abrirMenu();
+
+  }
+
+  abrirMenu() {
+
+    document.getElementById('mySidebar').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+
+  }
+
+  cerrarMenu() {
+
+    document.getElementById('mySidebar').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+
+  }
+
+  accionarMenu(event) {
+
+    if (this.menu.accionMenu === true) {
+
+      this.cerrarMenu();
+      this.menu.accionMenu = false;
+      this.menu.icono = 'fas fa-bars';
+
+    } else if (this.menu.accionMenu === false) {
+
+      this.abrirMenu();
+      this.menu.accionMenu = true;
+      this.menu.icono = 'far fa-times-circle';
+
+    }
+
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './Components/login/login.component';
+import { LoginComponent } from './Models/login/login.component';
+import { HomeComponent } from './Models/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
