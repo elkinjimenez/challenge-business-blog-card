f968ac2c1ad4eb001bf1d885ba57bc29cd19d607
elkinjimenez <elkin.jiga@hotmail.com>
2020-02-16 23:08:05 -0500

    Inicio de tabla de empleados, nuevo empleado y  carga masiva

--- src/app/Modules/empleados/empleados.module.ts
+++ src/app/Modules/empleados/empleados.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmpleadosComponent } from './empleados/empleados.component';
+import { NuevoComponent } from './nuevo/nuevo.component';
+import { HomeRoutingModule } from '../home-routing.module';
+import { CargaMasivaComponent } from './carga-masiva/carga-masiva.component';
 
 @NgModule({
-  declarations: [EmpleadosComponent],
+  declarations: [EmpleadosComponent, NuevoComponent, CargaMasivaComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HomeRoutingModule
   ],
   exports: [
     EmpleadosComponent
