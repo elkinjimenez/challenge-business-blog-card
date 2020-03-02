aafe84c7a660aae8e934afa4289eb90e829f948d
elkinjimenez <elkin.jiga@hotmail.com>
2020-03-01 22:54:21 -0500

    Mejorando la interfaz de proyecto

--- src/app/Model/proyectos/proyecto.ts
+++ src/app/Model/proyectos/proyecto.ts
@@ -1,4 +1,22 @@
 export interface Proyecto {
 
+  codigo: string;
+  nombre: string;
+  cliente: string;
+  fechaInicioContractual: string;
+  fehchaFinContractual: string;
+  duracionProyecto: string;
+  servicioAlcance: string;
+  ocContrato: string;
+  nContratoOc: string;
+  moneda: string;
+  valorTotalAdjudicado: string;
+  valorFormalizadoHost: string;
+  valorUltimoContrato: string;
+  tipoTarifa: string;
+  tarifa: string;
+  estadoProyecto: string;
+  subproyecto: string;
+  estadoContrato: string;
 
 }
