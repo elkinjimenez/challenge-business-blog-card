110f000b5d430900f9bd882628fbf96db8fecee7
elkinjimenez <elkin.jiga@hotmail.com>
2020-03-03 21:35:45 -0500

    Creando componente para nueva novedad

--- src/app/Modules/novedades/novedades.module.ts
+++ src/app/Modules/novedades/novedades.module.ts
@@ -4,9 +4,10 @@ import { NovedadesComponent } from './novedades/novedades.component';
 import { IncapacidadesComponent } from './incapacidades/incapacidades.component';
 import { OtrasNovedadesComponent } from './otras-novedades/otras-novedades.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
+import { NuevaNovedadComponent } from './nueva-novedad/nueva-novedad.component';
 
 @NgModule({
-  declarations: [NovedadesComponent, IncapacidadesComponent, OtrasNovedadesComponent],
+  declarations: [NovedadesComponent, IncapacidadesComponent, OtrasNovedadesComponent, NuevaNovedadComponent],
   imports: [
     CommonModule,
     AppRoutingModule
---
+++ src/app/Modules/novedades/nueva-novedad/nueva-novedad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nueva-novedad',
+  templateUrl: './nueva-novedad.component.html',
+  styleUrls: ['./nueva-novedad.component.css']
+})
+export class NuevaNovedadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
